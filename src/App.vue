f4eaed70ac0f477597e1ad80e6fb4fb3e52a1707
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-29 15:49:19 +0200

    create a component navbar and move the navbar code from app.vue to navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,14 @@
+
 <template>
 <!-- FONT AWESOME -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- GOOGLE FONT OPEN SANS -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet"> 
-
-  <div id="nav">
-  <header>
-      <div class="logo_container">
-        <li><router-link to="/"><i class="fab fa-accusoft logo"></i></router-link></li>
-      </div>
-      <nav>
-        <ul class="nav_links">
-          <li><router-link to="/">Acceuil</router-link></li>
-          <li><router-link to="/services">Services</router-link></li>
-          <li><router-link to="/competence">Compétences</router-link></li>
-          <li><router-link to="/portfolio">Portfolio</router-link></li>
-          <li><router-link to="/tarif">Tarifs</router-link></li>
-          <li><router-link to="/apropos">À propos</router-link></li>
-        </ul>
-      </nav>
-      <router-link to="/contact"><a class="cta"><button>contact</button></a></router-link>
-    </header>
-  </div>
-  <router-view/>
+<Navbar></Navbar>
 </template>
 
 <style>
-
 /* GENARALE STYLE */
 * {
   margin: 0;
@@ -36,65 +17,15 @@
   text-decoration: none;
   list-style-type: none;
 }
-
-/* NAVBAR STYLE */
-li,
-a,
-button {
-  font-family: "Open Sans", sans-serif;
-  font-weight: 600;
-  font-size: 16px;
-  color: #26332d;
-  text-decoration: none;
-}
-
-header {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 20px 10%;
-}
-
-.logo {
-  cursor: pointer;
-  font-size: 50px;
-  color: #42b983;
-}
-
-.logo_container {
-  margin-right: auto;
-}
-
-.nav_links {
-  list-style: none;
-}
-
-.nav_links li {
-  display: inline-block;
-  padding: 0px 20px;
-}
-
-.nav_links li a {
-  transition: all 0.3s ease 0s;
-}
-
-.nav_links li a:hover {
-  color: #42b983;
-  transition: all 0.3s ease 0s;
-}
-
-button {
-  margin-left: 20px;
-  padding: 9px 25px;
-  background-color: #42b983;
-  border: none;
-  border-radius: 50px;
-  cursor: pointer;
-  transition: all 0.3s ease 0;
-  color: white
-}
-
-button:hover {
-  background-color: #42b983dc;
-}
 </style>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="nav">
+  <header>
+      <div class="logo_container">
+        <li><router-link to="/"><i class="fab fa-accusoft logo"></i></router-link></li>
+      </div>
+      <nav>
+        <ul class="nav_links">
+          <li><router-link to="/">Acceuil</router-link></li>
+          <li><router-link to="/services">Services</router-link></li>
+          <li><router-link to="/competence">Compétences</router-link></li>
+          <li><router-link to="/portfolio">Portfolio</router-link></li>
+          <li><router-link to="/tarif">Tarifs</router-link></li>
+          <li><router-link to="/apropos">À propos</router-link></li>
+        </ul>
+      </nav>
+      <router-link to="/contact"><a class="cta"><button>contact</button></a></router-link>
+    </header>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+li,
+a,
+button {
+  font-family: "Open Sans", sans-serif;
+  font-weight: 600;
+  font-size: 16px;
+  color: #26332d;
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 20px 10%;
+}
+
+.logo {
+  cursor: pointer;
+  font-size: 50px;
+  color: #42b983;
+}
+
+.logo_container {
+  margin-right: auto;
+}
+
+.nav_links {
+  list-style: none;
+}
+
+.nav_links li {
+  display: inline-block;
+  padding: 0px 20px;
+}
+
+.nav_links li a {
+  transition: all 0.3s ease 0s;
+}
+
+.nav_links li a:hover {
+  color: #42b983;
+  transition: all 0.3s ease 0s;
+}
+
+button {
+  margin-left: 20px;
+  padding: 9px 25px;
+  background-color: #42b983;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+  transition: all 0.3s ease 0;
+  color: white
+}
+
+button:hover {
+  background-color: #42b983dc;
+}</style>
