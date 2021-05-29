3471a9d2a1e47a96838051b50ce610ccda03aaeb
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-05-29 15:28:50 +0200

    change the font for the navbar to open sans

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
+<!-- FONT AWESOME -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- GOOGLE FONT OPEN SANS -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet"> 
+
   <div id="nav">
   <header>
       <div class="logo_container">
@@ -32,12 +37,11 @@
   list-style-type: none;
 }
 
-
 li,
 a,
 button {
-  font-family: "Montserrat", sans-serif;
-  font-weight: 500;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 600;
   font-size: 16px;
   color: #26332d;
   text-decoration: none;
